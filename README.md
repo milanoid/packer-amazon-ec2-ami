9920879018f44b3ce371f015d3f289a997d08c72
Milan Vojnovic <milan.vojnovic@zonky.cz>
2017-09-22 09:32:59 +0200

    build pipeline using travis-c

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # packer-amazon-ec2-ami
+
+works with aws user rights 'AmazonEC2FullAccess'
--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
+packer -v
 packer validate example.json
-SET AWS_ACESS_KEY=your-access-key
-SET AWS_SECRET_KEY=your-secret-key
-packer build -var 'aws_access_key=$AWS_ACESS_KEY' -var 'aws_secret_key=$AWS_SECRET_KEY$' example.json
+AWS_ACESS_KEY=your-access-key
+AWS_SECRET_KEY=your-secret-key
+#packer build -var 'aws_access_key=$AWS_ACESS_KEY' -var 'aws_secret_key=$AWS_SECRET_KEY$' example.json
